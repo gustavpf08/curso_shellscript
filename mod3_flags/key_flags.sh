bdaac16846a3fb6d5a7b50d6a43919d2e97526c7
Gustavo Fausto <gugapf08@gmail.com>
2026-01-22 10:37:20 -0300

    controle de fluxo com flags

---
+++ mod3_flags/key_flags.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+#
+# listaUsuarios.sh - Extrai usuários do /etc/passwd
+#
+# Autor:      Gustavo Paulino
+# Manutenção: Gustavo Paulino
+#
+# ------------------------------------------------------------------------ #
+#  Este programa irá extrair os usuários do /etc/passwd, podendo listá-los
+#  em maiúsculo ou em ordem alfabética.
+#
+#  Exemplos:
+#      $ ./listaUsuarios.sh -s -m
+#      Neste exemplo, o script ficará em maiúsculo e em ordem alfabética
+# ------------------------------------------------------------------------ #
+# Histórico:
+#
+#   v1.0 21/01/2026, Gustavo:
+#     - Adição dos parâmetros -h, -v, -m e -a
+#   v1.1 21/01/2026, Gustavo:
+#     - Refactor do código adicionando chave valores.
+#   v1.2 21/01/2026, Gustavo:
+#     - Adicionando combinação de parâmetros, tendo comportamentos em conjunto.
+# ------------------------------------------------------------------------ #
+# Testado em:
+#   bash 5.2.37
+# ------------------------------- VARIÁVEIS ----------------------------------------- #
+
+USUARIOS="$(cat /etc/passwd | cut -d : -f 1)"
+MENSAGEM_USO=("
+  $(basename $0) - [OPÇÕES]
+  
+  -h - Menu de ajuda
+  -v - Versão
+  -s - Letra maiúscula
+  -m - Ordem Alfabética
+")
+VERSAO="v1.0"
+CHAVE_MAIUSCULO=0
+CHAVE_ORDEM=0
+
+# ------------------------------- EXECUÇÃO ----------------------------------------- #
+
+# O shift desloca os próximos parâmetros para a primeira posição.
+while test -n "$1"; do
+  case "$1" in
+  -h) echo "$MENSAGEM_USO" && exit 0 ;;
+  -v) echo "$VERSAO" && exit 0 ;;
+  -m) CHAVE_MAIUSCULO=1 ;;
+  -a) CHAVE_ORDEM=1 ;;
+  *) echo "Opção inválida. Valide no -h." && exit 1 ;;
+  esac
+  shift
+done
+# ------------------------------------------------------------------------ #
+
+[[ $CHAVE_MAIUSCULO -eq 1 ]] && USUARIOS=$(echo "${USUARIOS^^}")
+[[ $CHAVE_ORDEM -eq 1 ]] && USUARIOS=$(echo "$USUARIOS" | sort)
+
+echo "$USUARIOS"
